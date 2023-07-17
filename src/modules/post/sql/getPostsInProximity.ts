5c0bb6a8083232c7f70bc67bc7c51daa359d974b
Alec Reser <reser13@gmail.com>
2023-07-16 22:57:13 -0400

    Fixed location radius bug

--- src/modules/post/sql/getPostsInProximity.ts
+++ src/modules/post/sql/getPostsInProximity.ts
@@ -13,11 +13,12 @@ export const getPostsInProximity = ({
   take = 20,
   offset = 0,
 }: QueryPayload) => {
+  const radiusInMeters = radiusInKm * 1000;
   return Prisma.sql`
   SELECT 
     p.id
   FROM "Post" p
-  WHERE ST_DWithin(ST_MakePoint(longitude, latitude), ST_MakePoint(${longitude}, ${latitude})::geography, ${radiusInKm})
+  WHERE ST_DWithin(ST_MakePoint(longitude, latitude), ST_MakePoint(${longitude}, ${latitude})::geography, ${radiusInMeters})
   ORDER BY p."createdAt" DESC
   LIMIT ${take}
   OFFSET ${offset}`;
